ce7726229639050198f526ce727a3b9b35f01161
Nick Lyu <nick.lyu@mail.ru>
2023-11-13 23:42:02 +0300

    Fixing alive neighbors count...

--- Game.py
+++ Game.py
@@ -5,16 +5,16 @@ class Game:
 
     def __init__(self, n, m, rnd=False):
         if rnd:
-            self.current_board = [[Cell(rnd_alive=True) for i in range(n)] for j in range(m)]
+            self.current_board = [[Cell(rnd_alive=True) for j in range(n)] for i in range(m)]
         else:
-            self.current_board = [[Cell for i in range(n)] for j in range(m)]
+            self.current_board = [[Cell for j in range(n)] for i in range(m)]
 
     def next_gen(self):
+        alive_neighbors_board = self.get_alive_neighbors_board()
         for i in range(len(self.current_board)):
             for j in range(len(self.current_board[i])):
                 cell = self.current_board[i][j]
-                alive_neighbors = self.get_alive_neighbors(i, j)
-                # выставить соответствующий статус
+                alive_neighbors = alive_neighbors_board[i][j]
                 if cell.alive:
                     if alive_neighbors < 2 or alive_neighbors > 3:
                         cell.die()
@@ -25,12 +25,26 @@ class Game:
                         cell.born()
         # return self.current_board
 
+    def get_alive_neighbors_board(self):
+        alive_neighbors_board = [
+            [
+                self.get_alive_neighbors(i, j) for j in range(len(self.current_board[i]))
+            ] for i in range(len(self.current_board))
+        ]
+        return alive_neighbors_board
+
     def get_alive_neighbors(self, x, y):
+
         alive_count = 0
-        for i in range(x-1, x+1):
-            for j in range(y-1, y+1):
+
+        for i in range(x-1, x+2):
+            for j in range(y-1, y+2):
                 if not (i == x and j == y):
                     current_cell = self.current_board[i][j]
                     if current_cell.alive:
                         alive_count += 1
         return alive_count
+
+    def normalize_ranges(self, x, y):
+        if x == len(self.current_board) - 1:
+            a = 0
--- main.py
+++ main.py
@@ -11,19 +11,28 @@ def console_print_game(board, alive_cell_symbol='■', dead_cell_symbol='□'):
         print(*elem)
 
 
+def console_print_neighbors_board(board):
+    for elem in board:
+        print(*elem)
+
 def main():
     clear = lambda: os.system('clear')
-    my_game = Game(6, 5, rnd=True)
+    my_game = Game(5, 5, rnd=True)
     gens = 10
     console_print_game(my_game.current_board)
-    for i in range(gens):
-        my_game.next_gen()
-        # clear()
-        print()
-        console_print_game(my_game.current_board)
-        # sleep(1)
+    console_print_neighbors_board(my_game.get_alive_neighbors_board())
+
+    # for i in range(gens):
+    #     my_game.next_gen()
+    #     # clear()
+    #     print()
+    #     console_print_game(my_game.current_board)
+    #     # sleep(1)
 
 
 if __name__ == '__main__':
-    main()
-
+    # main()
+    a = [i for i in range(4)]
+    b = a[2:1]
+    print(a)
+    print(b)
